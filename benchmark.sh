7eafa5c412ba2202512d0801a5cf233092cbc201
dgbogi564 <82741370+dgbogi564@users.noreply.github.com>
2022-03-10 16:41:37 -0500

    benchmark.sh: Implemented

---
+++ benchmark.sh
@@ -0,0 +1,41 @@
+#!/bin/bash
+
+MAKE_PATH="/common/home/dog12/Documents/College/SPRING 2022/[01-198-415-01] COMPILERS/assignments/project1/C"
+EXEC_PATH="/common/home/dog12/Documents/College/SPRING 2022/[01-198-415-01] COMPILERS/assignments/project1/C/schedule"
+BENCHMARK_PATH="/common/home/dog12/Documents/College/SPRING 2022/[01-198-415-01] COMPILERS/assignments/project1/benchmarks"
+OUTPUT_PATH="/common/home/dog12/Documents/College/SPRING 2022/[01-198-415-01] COMPILERS/assignments/project1/output"
+
+cd "$MAKE_PATH" || exit
+make compile > /dev/null || exit
+cd "$BENCHMARK_PATH" || exit
+rm -rf "$OUTPUT_PATH"
+mkdir "$OUTPUT_PATH"
+
+
+printf "Running benchmarks...\n"
+
+rm -f "$OUTPUT_PATH/output.csv"
+echo "unoptimized,a,b,c" >> "$OUTPUT_PATH/output.csv"
+
+shopt -s nullglob
+for i in *.iloc; do
+  ~uli/cs415/ILOC_Simulator/sim < "$i" > "$OUTPUT_PATH/${i%%.*}.out";
+  uo=$(grep -oP '[0-9]* (?=cycles)' "$OUTPUT_PATH/${i%%.*}.out")
+
+  "$EXEC_PATH" "-a" < "$i" > "$OUTPUT_PATH/${i%%.*}.a.iloc" 2> /dev/null;
+  ~uli/cs415/ILOC_Simulator/sim < "$OUTPUT_PATH/${i%%.*}.a.iloc" > "$OUTPUT_PATH/${i%%.*}.a.out";
+  a=$(grep -oP '[0-9]* (?=cycles)' "$OUTPUT_PATH/${i%%.*}.a.out")
+
+  "$EXEC_PATH" "-b" < "$i" > "$OUTPUT_PATH/${i%%.*}.b.iloc" 2> /dev/null;
+  ~uli/cs415/ILOC_Simulator/sim < "$OUTPUT_PATH/${i%%.*}.b.iloc" > "$OUTPUT_PATH/${i%%.*}.b.out";
+  b=$(grep -oP '[0-9]* (?=cycles)' "$OUTPUT_PATH/${i%%.*}.b.out")
+
+  "$EXEC_PATH" "-c" < "$i" > "$OUTPUT_PATH/${i%%.*}.c.iloc" 2> /dev/null;
+  ~uli/cs415/ILOC_Simulator/sim < "$OUTPUT_PATH/${i%%.*}.c.iloc" > "$OUTPUT_PATH/${i%%.*}.c.out";
+  c=$(grep -oP '[0-9]* (?=cycles)' "$OUTPUT_PATH/${i%%.*}.c.out")
+
+  echo "$uo,$a,$b,$c" >> "$OUTPUT_PATH/output.csv"
+done
+
+
+printf "Benchmarks completed\n"
