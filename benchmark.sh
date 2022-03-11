f73ab5eb41496758db7f2c572d7c184acacfec41
dgbogi564 <82741370+dgbogi564@users.noreply.github.com>
2022-03-10 19:29:05 -0500

    benchmark.sh: fixed spacing

--- benchmark.sh
+++ benchmark.sh
@@ -20,19 +20,19 @@ echo "unoptimized,a,b,c" >> "$OUTPUT_PATH/output.csv"
 shopt -s nullglob
 for i in *.iloc; do
   ~uli/cs415/ILOC_Simulator/sim < "$i" > "$OUTPUT_PATH/${i%%.*}.out";
-  uo=$(grep -oP '[0-9]* (?=cycles)' "$OUTPUT_PATH/${i%%.*}.out")
+  uo=$(grep -oP '[0-9]*(?= cycles)' "$OUTPUT_PATH/${i%%.*}.out")
 
   "$EXEC_PATH" "-a" < "$i" > "$OUTPUT_PATH/${i%%.*}.a.iloc" 2> /dev/null;
   ~uli/cs415/ILOC_Simulator/sim < "$OUTPUT_PATH/${i%%.*}.a.iloc" > "$OUTPUT_PATH/${i%%.*}.a.out";
-  a=$(grep -oP '[0-9]* (?=cycles)' "$OUTPUT_PATH/${i%%.*}.a.out")
+  a=$(grep -oP '[0-9]*(?= cycles)' "$OUTPUT_PATH/${i%%.*}.a.out")
 
   "$EXEC_PATH" "-b" < "$i" > "$OUTPUT_PATH/${i%%.*}.b.iloc" 2> /dev/null;
   ~uli/cs415/ILOC_Simulator/sim < "$OUTPUT_PATH/${i%%.*}.b.iloc" > "$OUTPUT_PATH/${i%%.*}.b.out";
-  b=$(grep -oP '[0-9]* (?=cycles)' "$OUTPUT_PATH/${i%%.*}.b.out")
+  b=$(grep -oP '[0-9]*(?= cycles)' "$OUTPUT_PATH/${i%%.*}.b.out")
 
   "$EXEC_PATH" "-c" < "$i" > "$OUTPUT_PATH/${i%%.*}.c.iloc" 2> /dev/null;
   ~uli/cs415/ILOC_Simulator/sim < "$OUTPUT_PATH/${i%%.*}.c.iloc" > "$OUTPUT_PATH/${i%%.*}.c.out";
-  c=$(grep -oP '[0-9]* (?=cycles)' "$OUTPUT_PATH/${i%%.*}.c.out")
+  c=$(grep -oP '[0-9]*(?= cycles)' "$OUTPUT_PATH/${i%%.*}.c.out")
 
   echo "$uo,$a,$b,$c" >> "$OUTPUT_PATH/output.csv"
 done
